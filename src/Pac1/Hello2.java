903a697fb6a9d0a1cabf847645b045616ee4492d
Prateek <prateeksharma104@gmail.com>
2018-12-02 00:53:22 +0530

    This is out first commit, where iu add Hello.java

---
+++ src/Pac1/Hello2.java
@@ -0,0 +1,11 @@
+package Pac1;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi this is the prog ram i want to rum");
+		
+		System.out.println("Hahahah");
+	}
+
+}
