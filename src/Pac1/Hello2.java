f2eac548f259fd2f4e5c8f16691ddb5545e1c8fc
Prateek <prateeksharma104@gmail.com>
2018-12-03 00:17:09 +0530

    This is a commit, where i have added a loop in Hello2.java

--- src/Pac1/Hello2.java
+++ src/Pac1/Hello2.java
@@ -3,9 +3,13 @@ package Pac1;
 public class Hello2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi this is the prog ram i want to rum");
 		
-		System.out.println("Hahahah");
+		for(int i=0;i<10;i++) {
+			
+		System.out.println(" the count is "+i);
+		
+		}
+		
 	}
 
 }
